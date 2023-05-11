df9ad1ea10107467d461468d77c00bcb015622dd
Merchant6 <saboormerchant6@gmail.com>
2023-05-11 19:59:47 +0500

    Implemented Registeration using Passport Auth

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\ResgisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::middleware(['json'])->group(function(){
+    Route::post('/register', [ResgisterController::class, 'store']);
 });
+
